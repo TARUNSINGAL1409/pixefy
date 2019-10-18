fd93797aa48035c2f132af11adb1e1c40ebc8d15
Tarun Singal <tarunsingal1409@gmail.com>
2019-10-19 01:42:01 +0530

    Starting Layout

---
+++ src/CategoryList.js
@@ -0,0 +1,18 @@
+import React from 'react' ; 
+import Card from './Card' ; 
+
+const CategoryList = ({categories}) => {
+	return( 
+       <div>
+         {
+         	categories.map(categ => {
+         		return (
+         			 <Card id = {categ.id} name={categ.category} />
+                ) ;
+         	})
+         }
+       </div> 
+	) ;
+}
+
+export default CategoryList ; 
---
+++ src/categories.js
@@ -0,0 +1,51 @@
+export const categories = [
+ {
+ 	id : 1 ,
+ 	category :'Nature'  
+ } , 
+ {
+    id : 2 ,
+    category :'Wildlife'  
+ },
+ {
+    id : 3 ,
+    category :'Festival'
+ },
+ {
+    id : 4 ,
+    category :'Celebs' 
+ },
+ {
+    id : 5 ,
+    category :'Technology' 
+ },
+ {
+    id : 6 ,
+    category :'Animated'  
+ },
+ {
+    id : 7 ,
+    category :'Automobiles'
+},
+{
+   id : 8 ,
+   category :'Quotes'
+},
+{
+  id : 9 ,
+  category :'Logos'
+},
+{
+    id : 10 ,
+    category :'God'
+},
+{
+    id : 11 ,
+    category :'Monuments'
+},
+{
+    id : 12 ,
+    category :'Adventure'
+},
+
+] ; 
