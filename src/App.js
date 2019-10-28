e5b521cf260fc892c533c9c7509890ae8c172681
aryantanny <aryantan1999@gmail.com>
2019-10-28 10:41:05 +0530

    arranged the files in order and added the code for signin form and registration form

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React , {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import CategoryList from './CategoryList' ; 
-import {categories} from './categories' ; 
+import CategoryList from './Component/CategoryList/CategoryList' ; 
+import {categories} from './Component/categories/categories' ;
+import Navigation from './Component/Navigation/Navigation';
+import Register from './Component/Register/Register';
+import Signin from './Component/Signin/Signin';
 
 class App extends Component {
   render() {
       return (
         <div className="tc">
            <h1 className = 'f1'> PIXEFY  </h1>
-            <div> 
+            <div>
+                {/*<Navigation />
+                <Signin />
+                <Register />*/}
                 <CategoryList categories = {categories} /> 
             </div>     
         </div>
